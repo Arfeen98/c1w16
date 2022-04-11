93fc96534840aa8e26d8e8e45a65ea816224145d
Arfeen98 <98758325+Arfeen98@users.noreply.github.com>
2022-04-11 14:18:40 +0530

    minor changes

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -1,15 +1,44 @@
-
+import React from "react";
+import '../App.css';
 // keep the add to cart component here
 const CartButton = () => {
     //manage state of the count 
-  return <>
-  {/* add to cart button */}
-  {/* count with - and  + button */}
-  <div>
-      <button>-</button>
-      {/* <p className="count-item">{count}</p> */}
-      <button>+</button>
-  </div>
-  </>;
+    // let [count,setCount]=React.useState[0];
+    // const handleClick=(e)=>{
+    //   console.log(count);
+    //   setCount(e.target.count++);
+    // }
+    let [count,setCount]=React.useState(0);
+    const handleClick=(e)=>{
+      setCount(++count);
+    }
+    const handleLow=(e)=>{
+      setCount(--count);
+    }
+    if(count===0){
+      var a="Add to Cart";
+    }
+    else{
+      a="+";
+    }
+    if(count===0){
+      var b;
+    }
+    else{
+      b="-";
+    }
+  return( 
+    <>
+    {/* add to cart button */}
+    {/* count with - and  + button */}
+    <div className="btn">
+        <button onClick={handleLow}>{b}</button>
+        <p className="count-item">{count}</p>
+        <button onClick={handleClick}>{a}</button>
+    </div>
+    </>
+  );
+   
+  
 };
 export default CartButton
--- src/components/Grocerydetails.jsx
+++ src/components/Grocerydetails.jsx
@@ -1,15 +1,32 @@
 import React from "react"
 import data from '../data.json'
-
+import '../App.css';
+import CartButton from "./CartButton";
 
 const GroceryDetails = ()=>{
-console.log(data)
+//console.log(data)
 
     return(
         <>
         <h1>Groceries</h1>
         <div className="container">
+            {data.map((elem)=>{
+      //  console.log(elem.id);
+        return(
+            <div key={elem.id}>
+                <img src={elem.imgURL} alt="" />
+                <h4>{elem.title}</h4>
+                <h4>{elem.mrp}</h4>
+                <h4>{elem.sellingPrice}</h4>
+                <h4>{elem.discount}</h4>
+                <CartButton/>
+            </div>
+            
+        )
+
+       })}
             {/* map through the data and display the cards */}
+
         </div>
         </>
     )
